3caf635419175f62ec284f5154f54e176ee5af25
solstc <solc.carrasco@gmail.com>
2022-11-17 12:07:23 -0300

    GBF extraction

---
+++ 02_EXTRACT_GBF.R
@@ -0,0 +1,25 @@
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+# source('00_INITIALIZATION.R')
+
+# Phage selection by id ----
+# my_ids = Gi of phages
+my_ids <- read.xlsx2('../data/database_info.xlsx', 1, header = T)[, 2] 
+
+# Search and retrieve GBK ----
+
+setwd('../data/gbk')
+
+for (seq_start in my_ids) {
+  all_recs <-
+    entrez_fetch(db = 'nuccore', id = seq_start, rettype = 'gb')
+  write.table(
+    all_recs,
+    file = paste0(seq_start, '.gbf'),
+    sep = ",",
+    row.names = F,
+    col.names = F,
+    qmethod = "double",
+    quote = F
+  )
+}
+print('COMPLETED!')
